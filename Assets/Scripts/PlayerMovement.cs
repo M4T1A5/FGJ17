8db2619406980ed911078e544818a9cd3a3de824
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2017-01-20 21:14:54 +0200

    Changed player to playermovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed = 10;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+}
