c8c6d176bc07a5e0a983a56856a0e4502b7a1b34
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2017-01-21 02:21:34 +0200

    Add mockup score system integration

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,15 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    public scoreSystem ScoreSystem;
+
     private void Awake()
     {
         if (Instance == null)
             Instance = this;
         else
             Destroy(gameObject);
+
+        ScoreSystem = GetComponent<scoreSystem>();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,30 @@ public class Player : MonoBehaviour
 {
     public Transform LastHit { get; set; }
 
-    void Start()
+    private void Start()
     {
 
     }
 
-    void Update()
+    private void Update()
     {
         if (transform.position.y < -10)
         {
             // Player has fallen out of level for sure
             Destroy(gameObject);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+            LastHit = collision.transform;
+    }
+
+    private void OnDestroy()
+    {
+        // TODO: Add points to the player that killed us
+        //var playerId = (int) LastHit.GetComponent<PlayerMovement>().PlayerIndex;
+        //GameManager.Instance.ScoreSystem.updateScore(playerId, 1);
     }
 }
