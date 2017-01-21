672e85f9467649aca72772290e4a8b36d2247e49
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2017-01-21 15:49:05 +0200

    Add player spawning script

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject PlayerPrefab;
+
+    private Transform[] spawnPoints;
+    private readonly List<int> usedSpawns = new List<int>();
+
+    private void Start()
+    {
+        spawnPoints = transform.GetComponentsInChildren<Transform>().Skip(1).ToArray();
+
+        var playerCount = GameManager.Instance.PlayerCount;
+
+        for (int i = 0; i < playerCount; i++)
+        {
+            var foundSpawn = false;
+            do
+            {
+                var spawn = Random.Range(0, spawnPoints.Length);
+                if(usedSpawns.Contains(spawn))
+                    continue;
+
+                usedSpawns.Add(spawn);
+                Instantiate(PlayerPrefab, spawnPoints[spawn].position, Quaternion.identity);
+                foundSpawn = true;
+            } while (!foundSpawn);
+        }
+    }
+
+    public void RequestRespawn(Transform player, int playerId)
+    {
+        player.position = spawnPoints[usedSpawns[playerId]].position;
+        player.rotation = Quaternion.identity;
+    }
+}
