62497eabb746e9598f3fcd7133b19db5a602e895
Matias Kangasjärvelä <matias.kangasjarvela@gmail.com>
2017-01-21 02:12:09 +0200

    Start adding some global state to the game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public Transform LastHit { get; set; }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (transform.position.y < -10)
+        {
+            // Player has fallen out of level for sure
+            Destroy(gameObject);
+        }
+    }
+}
